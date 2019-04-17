07e8a49bb5914c57a46c09a78f21f9ab247ebfc9
zhanjingbo <412008196@qq.com>
2019-04-17 11:21:07 +0800

    20190417

---
+++ src/Problem66.java
@@ -0,0 +1,19 @@
+/**
+ * leetcode-66 加一
+ *
+ * @link {https://leetcode-cn.com/problems/plus-one/}
+ */
+public class Problem66 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
