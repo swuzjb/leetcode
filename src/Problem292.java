13e3a3e27149d24ec77e64466cc968468c7aa5e5
zhanjingbo <412008196@qq.com>
2019-05-06 19:48:11 +0800

    20190506

---
+++ src/Problem292.java
@@ -0,0 +1,9 @@
+/**
+ * leetcode292
+ * @link {https://leetcode-cn.com/problems/nim-game/}
+ */
+public class Problem292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
